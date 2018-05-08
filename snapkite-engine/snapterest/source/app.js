43e0f5da9b1671781f0c325ed9214ec480bad4b8
csp5096 <christopherscpowell@gmail.com>
2018-05-08 14:17:16 -0500

    Add application component

--- snapkite-engine/snapterest/source/app.js
+++ snapkite-engine/snapterest/source/app.js
@@ -1,50 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import Application from './components/Application';
 
-class ReactClass extends Component {
-  state = {
-    isHidden: false
-  };
-
-  handleClick = () => {
-    this.setState(prevState => ({
-      isHeaderHidden: !prevState.isHeaderHidden
-    }));
-  }
-
-  render () {
-    const {
-      isHeaderHidden,
-    } = this.state;
-
-    if (isHeaderHidden) {
-      return (
-        <button
-          className="btn ban-default"
-          onClick={this.handleClick}
-        >
-          Toggle Header
-        </button>
-      );
-    }
-
-    return (
-      <div>
-        <h1 className="header">React Component</h1>
-        <button
-          className="btn ban-default"
-          onClick={this.handleClick}
-        >
-          Toggle Header
-        </button>
-      </div>
-    );
-  }
-}
-
-const reactComponent = ReactDOM.render(
-  <ReactClass/ >,
+ReactDOM.render(
+  <Application />,
   document.getElementById('react-application')
 );
-
-export default ReactClass;
