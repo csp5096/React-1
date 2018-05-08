9845bd10d652080c90139150598a141c362babb6
csp5096 <christopherscpowell@gmail.com>
2018-05-08 13:56:26 -0500

    Added stateless feature button

--- snapkite-engine/snapterest/source/app.js
+++ snapkite-engine/snapterest/source/app.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const listOfItems = (
-  <ul className="list-of-items">
-    <li className="item-1">Item 1</li>
-    <li className="item-2">Item 2</li>
-    <li className="item-2">Item 3</li>
-  </ul>
-);
+class ReactClass extends Component {
+  state = {
+    isHidden: false
+  };
 
-ReactDOM.render(
-  listOfItems,
+  handleClick = () => {
+    this.setState(prevState => ({
+      isHeaderHidden: !prevState.isHeaderHidden
+    }));
+  }
+
+  render () {
+    const {
+      isHeaderHidden,
+    } = this.state;
+
+    if (isHeaderHidden) {
+      return (
+        <button
+          className="btn ban-default"
+          onClick={this.handleClick}
+        >
+          Toggle Header
+        </button>
+      );
+    }
+
+    return (
+      <div>
+        <h1 className="header">React Component</h1>
+        <button
+          className="btn ban-default"
+          onClick={this.handleClick}
+        >
+          Toggle Header
+        </button>
+      </div>
+    );
+  }
+}
+
+const reactComponent = ReactDOM.render(
+  <ReactClass/ >,
   document.getElementById('react-application')
 );
+
+export default ReactClass;
