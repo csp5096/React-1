54eff51c04b52892f6d6088befe495849d920787
csp5096 <christopherscpowell@gmail.com>
2018-05-11 13:29:33 -0500

    Introduced basic unit testing

---
+++ snapkite-engine/snapterest/source/utils/TweetUtils.test.js
@@ -0,0 +1,23 @@
+import TweetUtils from './TweetUtils';
+
+describe('Tweet utilities module', function () {
+  it('returns an array of tweet ids', function () {
+    const tweetsMock = {
+      tweet1: {},
+      tweet2: {},
+      tweet3: {}
+    };
+
+    const expectedListOfTweetIds = [
+      'tweet1',
+      'tweet2',
+      'tweet3'
+    ];
+    const actualListOfTweetIds = TweetUtils.getListOfTweetIds(
+      tweetsMock
+    );
+
+    expect(actualListOfTweetIds)
+      .toEqual(expectedListOfTweetIds);
+  });
+});
